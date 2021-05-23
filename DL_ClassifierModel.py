fix averaging error

DL_ClassifierModel.py
@@ -30,7 +30,7 @@ class BaseClassifier:
         data = np.concatenate((dataClass.eSeqData['train'], dataClass.eSeqData['valid']))
 
         results = []
-        results_train_val = {'train': [], 'test': []}
+        results_train_val = {'train': [], 'valid': []}
         for i, (train_index, test_index) in enumerate(skf.split(data)):
             dataClass.eSeqData['train'] = data[train_index]
             dataClass.eSeqData['valid'] = data[test_index]

metrics.py
@@ -66,7 +66,7 @@ class Metrictor:
         avg_cv = {'train': {}, 'valid': {}}
         for subset in ['train', 'valid']:
             for met in report:
-                avg_cv[subset][met] = [np.mean([res[met] for res in subset])]
+                avg_cv[subset][met] = [np.mean([res[met] for res in results])]
         return avg_cv
 
     def each_class_indictor_show(self, id2lab):